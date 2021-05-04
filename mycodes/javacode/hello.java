255344f236c509b5b164cac8bb7660752d3a6d3b
Ashutoshh <asashuthegreat4@gmail.com>
2021-05-04 12:54:36 +0530

    Add files via upload

---
+++ mycodes/javacode/hello.java
@@ -0,0 +1,10 @@
+class myclass { 
+    public static void main(String args[]) 
+    { 
+        // test expression 
+        while (true) { 
+            System.out.println("Hello World welcome to containers !!"); 
+  
+        } 
+    } 
+} 
---
+++ mycodes/pythoncode/hello.py
@@ -0,0 +1,9 @@
+import  time
+x="Hello Docker !!"
+
+while 3 > 2 :
+    print("Hey Ashutoshh")
+    time.sleep(2)
+    print(x)
+    print("_____________++++++_____")
+    time.sleep(1)
